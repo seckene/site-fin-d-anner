4dadd476e4e83d4b2eff5da4f47c5e6b921c20d9
Seckene Sako <seckene_sako@hotmail.com>
2025-07-29 13:48:06 +0200

    commitnumber8

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'register')]
+    public function register(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+            $hashedPassword = $hasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'Mot de passe',
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [new NotBlank(['message' => 'Entrez un mot de passe.'])]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
