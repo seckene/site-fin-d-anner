a1044d8ce846428cfeefff2d3d1a0b35788e7482
Seckene Sako <seckene_sako@hotmail.com>
2025-08-18 10:41:27 +0200

    commitnumber14

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
@@ -22,6 +23,11 @@ class UserType extends AbstractType
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Adresse e-mail',
+                'attr' => ['class' => 'form-control'],
+                'row_attr' => ['class' => 'mb-3'],
+            ])
+            ->add('tel', TextType::class, [   // 👈 AJOUT DU CHAMP TEL
+                'label' => 'Téléphone',
                 'attr' => ['class' => 'form-control'],
                 'row_attr' => ['class' => 'mb-3'],
             ])
@@ -42,12 +48,5 @@ class UserType extends AbstractType
                 'label_attr' => ['class' => 'form-check-label'],
                 'choice_attr' => fn () => ['class' => 'form-check-input'],
             ]);
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
     }
 }
